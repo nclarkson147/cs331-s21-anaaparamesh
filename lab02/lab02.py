52be519a5ff41e12a98502a55d0900a52a75b0f5
Akshaya Naapa Ramesh <akshaya.naapa@gamil.com>
2021-02-06 00:52:32 -0500

    implemented compute_ngrams  Changes to be committed: 	modified:   lab02.py

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -34,59 +34,91 @@ ROMEO_SOLILOQUY = """
 # Implement this function
 def compute_ngrams(toks, n=2):
     """Returns an n-gram dictionary based on the provided list of tokens."""
-    pass
+    # generate n_gram token
+    n_gram_list = [tuple(toks[i : i + n]) for i in range(len(toks) - (n - 1))]
+
+    # create dictionary
+    n_gram = {}
+
+    # run through each of the tuples in the list
+    for values in n_gram_list:
+        # if the first token in tuple is a key, then just append the other tokens, else add tokens to a new key.
+        if values[0] in n_gram.keys():
+            n_gram[values[0]].append(values[1:])
+        else:
+            n_gram[values[0]] = [tuple(values[1:])]
+    # return n_gram
+    return n_gram
+
 
 def test1():
     test1_1()
     test1_2()
 
+
 # 20 Points
 def test1_1():
     """A smaller test case for your ngram function."""
     tc = TestCase()
-    simple_toks = [t.lower() for t in 'I really really like cake.'.split()]
+    simple_toks = [t.lower() for t in "I really really like cake.".split()]
 
     compute_ngrams(simple_toks)
-    tc.assertEqual(compute_ngrams(simple_toks),
-                   {'i': [('really',)], 'like': [('cake.',)], 'really': [('really',), ('like',)]})
-    tc.assertEqual(compute_ngrams(simple_toks, n=3),
-                   {'i': [('really', 'really')],
-                    'really': [('really', 'like'), ('like', 'cake.')]})
+    tc.assertEqual(
+        compute_ngrams(simple_toks),
+        {"i": [("really",)], "like": [("cake.",)], "really": [("really",), ("like",)]},
+    )
+    tc.assertEqual(
+        compute_ngrams(simple_toks, n=3),
+        {
+            "i": [("really", "really")],
+            "really": [("really", "like"), ("like", "cake.")],
+        },
+    )
 
     romeo_toks = [t.lower() for t in ROMEO_SOLILOQUY.split()]
 
     dct = compute_ngrams(romeo_toks, n=4)
-    tc.assertEqual(dct['but'], [('sick', 'and', 'green'), ('fools', 'do', 'wear')])
-    tc.assertEqual(dct['it'],
-                   [('is', 'the', 'east,'),
-                    ('off.', 'it', 'is'),
-                    ('is', 'my', 'lady,'),
-                    ('is', 'my', 'love!'),
-                    ('were', 'not', 'night.')])
+    tc.assertEqual(dct["but"], [("sick", "and", "green"), ("fools", "do", "wear")])
+    tc.assertEqual(
+        dct["it"],
+        [
+            ("is", "the", "east,"),
+            ("off.", "it", "is"),
+            ("is", "my", "lady,"),
+            ("is", "my", "love!"),
+            ("were", "not", "night."),
+        ],
+    )
+
 
 # 30 Points
 def test1_2():
     """Test your code on Peter Pan."""
-    PETER_PAN_URL = 'https://moss.cs.iit.edu/cs331/data/peterpan.txt'
+    PETER_PAN_URL = "https://moss.cs.iit.edu/cs331/data/peterpan.txt"
     peter_pan_text = urllib.request.urlopen(PETER_PAN_URL).read().decode()
     tc = TestCase()
     pp_toks = [t.lower() for t in peter_pan_text.split()]
     dct = compute_ngrams(pp_toks, n=3)
-    tc.assertEqual(dct['crocodile'],
-                   [('passes,', 'but'),
-                    ('that', 'happened'),
-                    ('would', 'have'),
-                    ('was', 'in'),
-                    ('passed', 'him,'),
-                    ('is', 'about'),
-                    ('climbing', 'it.'),
-                    ('that', 'was'),
-                    ('pass', 'by'),
-                    ('and', 'let'),
-                    ('was', 'among'),
-                    ('was', 'waiting')])
-    tc.assertEqual(len(dct['wendy']), 202)
-    tc.assertEqual(len(dct['peter']), 243)
+    tc.assertEqual(
+        dct["crocodile"],
+        [
+            ("passes,", "but"),
+            ("that", "happened"),
+            ("would", "have"),
+            ("was", "in"),
+            ("passed", "him,"),
+            ("is", "about"),
+            ("climbing", "it."),
+            ("that", "was"),
+            ("pass", "by"),
+            ("and", "let"),
+            ("was", "among"),
+            ("was", "waiting"),
+        ],
+    )
+    tc.assertEqual(len(dct["wendy"]), 202)
+    tc.assertEqual(len(dct["peter"]), 243)
+
 
 ################################################################################
 # EXERCISE 2
@@ -95,23 +127,30 @@ def test1_2():
 def gen_passage(ngram_dict, length=100):
     pass
 
+
 # 50 Points
 def test2():
     """Test case for random passage generation."""
     tc = TestCase()
     random.seed(1234)
-    simple_toks = [t.lower() for t in 'I really really like cake.'.split()]
-    tc.assertEqual(gen_passage(compute_ngrams(simple_toks), 10),
-                   'like cake. i really really really really like cake. i')
+    simple_toks = [t.lower() for t in "I really really like cake.".split()]
+    tc.assertEqual(
+        gen_passage(compute_ngrams(simple_toks), 10),
+        "like cake. i really really really really like cake. i",
+    )
 
     random.seed(1234)
     romeo_toks = [t.lower() for t in ROMEO_SOLILOQUY.split()]
-    tc.assertEqual(gen_passage(compute_ngrams(romeo_toks), 10),
-                   'too bold, \'tis not night. see, how she leans her')
+    tc.assertEqual(
+        gen_passage(compute_ngrams(romeo_toks), 10),
+        "too bold, 'tis not night. see, how she leans her",
+    )
+
 
 def main():
     test1()
     test2()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
