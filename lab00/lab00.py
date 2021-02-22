6dc4b9e0a9f67a0487f28445caf902294aa5ac43
Boris Glavic <lordpretzel@gmail.com>
2021-02-22 17:48:09 -0600

    updates

--- lab00/lab00.py
+++ lab00/lab00.py
@@ -9,7 +9,7 @@ def main():
     assert pal.strip(), 'String should not be empty'
     assert len(pal) > 1, 'String should be at least 2 letters long'
     assert pal == pal[::-1], 'String is not a palindrome'
-    print("your successfully created a palindrome!"
+    print("your successfully created a palindrome!")
 
 if __name__ == '__main__':
     main()
